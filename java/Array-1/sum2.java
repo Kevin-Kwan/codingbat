0a9542534443c3ae18d50103fc933bd251ec6f80
Kevin <koolkev246@gmail.com>
2021-07-29 18:10:20 -0400

    Create sum2.java

---
+++ java/Array-1/sum2.java
@@ -0,0 +1,12 @@
+public int sum2(int[] nums) {
+  int sum=0;
+  if (nums.length==0)
+  sum=0;
+  else if (nums.length<2)
+  sum=nums[0];
+  else if (nums.length>=2)
+  sum=nums[0]+nums[1];
+  else
+  return sum;
+  return sum;
+}
