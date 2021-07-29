70285ea58b289ae856ddfeb277446434c572fde0
Kevin <koolkev246@gmail.com>
2021-07-29 18:14:01 -0400

    Create luckySum.java

---
+++ java/Logic-2/luckySum.java
@@ -0,0 +1,13 @@
+public int luckySum(int a, int b, int c) {
+  int sum=a+b+c;
+  if (a==13)
+  return 0;
+  else if (b==13)
+  return a;
+  else if (c==13)
+  return a+b;
+  else
+  return sum;
+  
+}
+
