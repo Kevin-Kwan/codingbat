44162d9249afa502f9da12a5d468266cb5daeca9
Kevin <koolkev246@gmail.com>
2021-07-29 18:19:40 -0400

    Create scoreUp.java

---
+++ java/AP-1/scoreUp.java
@@ -0,0 +1,19 @@
+public int scoreUp(String[] key, String[] answers) {
+  int score = 0;
+  for (int i=0;i<key.length;i++)
+  {
+   if(answers[i].equals("?"))
+   {
+     score=score;
+   }
+   else if(answers[i].equals(key[i]))
+   {
+    score += 4;
+   }
+   else
+   {
+    score -= 1;
+   }
+  }
+  return score;
+}
