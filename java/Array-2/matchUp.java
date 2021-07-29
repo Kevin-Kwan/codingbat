fb279282eab195a81cff993c57d2fa3a9721dda3
Kevin <koolkev246@gmail.com>
2021-07-29 18:56:25 -0400

    Create matchUp.java

---
+++ java/Array-2/matchUp.java
@@ -0,0 +1,7 @@
+public int matchUp(int[] nums1, int[] nums2) {
+  int times=0;
+  for (int i=0;i<nums1.length;i++)
+  if ((Math.abs(nums1[i]-nums2[i]))<=2&&(Math.abs(nums1[i]-nums2[i])!=0))
+  times++;
+  return times;
+}
