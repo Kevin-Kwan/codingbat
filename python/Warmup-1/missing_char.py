66702fa80340bb95404806900a44f3fe087eb30b
Kevin <koolkev246@gmail.com>
2021-08-03 20:42:28 -0400

    Create missing_char.py

---
+++ python/Warmup-1/missing_char.py
@@ -0,0 +1,7 @@
+def missing_char(str, n):
+  if len(str)>n:
+    return str[0:n]+str[n+1:]
+
+# https://thispointer.com/python-how-to-remove-characters-from-a-string-by-index/ 
+# https://www.w3schools.com/python/gloss_python_string_slice.asp
+# python slicing strings
